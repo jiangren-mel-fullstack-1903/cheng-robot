71f8eb19dbf0c93da41669f8812abd83938b1be3
changchengchang <46731883+changchengchang@users.noreply.github.com>
2019-04-05 14:13:37 +1100

    wrap logic into the game object (not working)

--- robot.js
+++ robot.js
@@ -1,67 +1,69 @@
-var DIRECTION = {
-    north: 'A', east: '->', south: 'V', west: '<-'
-};
-
-var state = {
-    robotPosition: {
-        x: 0, y: 0,
+var game = {
+    DIRECTION: {
+        north: 'A', east: '->', south: 'V', west: '<-'
     },
-    mapSize: {
-        x: 4, y: 4
-    }
-}
-
-function toIndex(position, mapSize) {
-    return position.x + position.y * mapSize.y;
-}
-
-function init() {
-    state.robotPosition.y = 3;
-}
-
-function clearAll() {
-    var cells = Array.prototype.slice.call(document.querySelectorAll('.map-cell'));
-    cells.map(function (aCell) {
-        aCell.innerHTML = '';
-    });
-    e.target.innerHTML = 'A';
-};
-
-function onCommandUp() {
-    move({ x: state.robotPosition.x, y: state.robotPosition.y - 1 });
-}
-
-function availablePosition(newPosition, mapSize) {
-    if (newPosition.x >= 0 && newPosition.x < mapSize.x
-        && newPosition.y >= 0 && newPosition.y < mapSize.y
-    ) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-function move(newPosition) {
-    if (availablePosition(newPosition, state.mapSize)) {
-        state.robotPosition = newPosition;
-        render();
-        return true;
-    } else {
-        return false;
-    }
-}
-
-function render() {
-    var mapCells = document.querySelectorAll('.map-cell');
-    var robotIndex = toIndex(state.robotPosition, state.mapSize);
-    for (var i = 0; i < mapCells.length; i++) {
-        if (i === robotIndex) {
-            mapCells[i].innerHTML = DIRECTION.north;
-        } else {
-            mapCells[i].innerHTML = '';
+    state: {
+        robotPosition: {
+            x: 0, y: 0,
+        },
+        mapSize: {
+            x: 4, y: 4
         }
+    },
+    toIndex(position, mapSize) {
+        return position.x + position.y * mapSize.y;
+    },
+    init() {
+        this.state.robotPosition.y = 3;
+    },
+    clearAll() {
+        var cells = Array.prototype.slice.call(document.querySelectorAll('.map-cell'));
+        cells.map(function (aCell) {
+            aCell.innerHTML = '';
+        });
+        e.target.innerHTML = 'A';
+    },
+    onCommandUp() {
+        this.move({ x: this.state.robotPosition.x, y: this.state.robotPosition.y - 1 });
+    },
+    availablePosition(newPosition, mapSize) {
+        if (newPosition.x >= 0 && newPosition.x < mapSize.x
+            && newPosition.y >= 0 && newPosition.y < mapSize.y
+        ) {
+            return true;
+        } else {
+            return false;
+        }
+    },
+    move(newPosition) {
+        if (this.availablePosition(newPosition, this.state.mapSize)) {
+            this.state.robotPosition = newPosition;
+            this.render();
+            return true;
+        } else {
+            return false;
+        }
+    },
+    render() {
+        var mapCells = document.querySelectorAll('.map-cell');
+        var robotIndex = this.toIndex(this.state.robotPosition, this.state.mapSize);
+        mapCells.forEach(function(aCell, i) {
+            if (i === robotIndex) {
+                mapCells[i].innerHTML = this.DIRECTION.north;
+            } else {
+                mapCells[i].innerHTML = '';
+            }
+        })
+        // for (var i = 0; i < mapCells.length; i++) {
+        //     if (i === robotIndex) {
+        //         mapCells[i].innerHTML = this.DIRECTION.north;
+        //     } else {
+        //         mapCells[i].innerHTML = '';
+        //     }
+        // }
     }
 }
 
-init();
-render();
+
+game.init();
+game.render();
