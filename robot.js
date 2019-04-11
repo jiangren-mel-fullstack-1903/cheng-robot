26a238f5aba18e6508ea5ccd05e29288f89b58c7
Cheng Chang <cheng.chang@team.telstra.com>
2019-04-11 16:02:36 +1000

    wrap into classes

--- robot.js
+++ robot.js
@@ -1,70 +1,101 @@
-var game = {
-    DIRECTION: {
-        north: 'A', east: '->', south: 'V', west: '<-'
-    },
-    state: {
-        robotPosition: {
-            x: 0, y: 0
-        },
-        mapSize: {
-            x: 4, y: 4
+class Position {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    getUpPosition() {
+        return new Position(this.x, this.y - 1);
+    }
+}
+
+class GameObject {
+    constructor(position, icon, map) {
+        this.position = position;
+        this.icon = icon;
+        this.map = map;
+    }
+}
+
+class MovingObject extends GameObject {
+    constructor(position, icon, map) {
+        super(position, icon, map);
+        this.history = [];
+    }
+    move(newPosition) {
+        if (this.map.availablePosition(newPosition)) {
+            this.history.push(this.position);
+            this.position = newPosition;
+            // this.state = Object.assign({}, this.state, {robotPosition: newPosition});
+            return true;
+        } else {
+            return false;
         }
-    },
-    history: [],
-    toIndex(position, mapSize) {
-        return position.x + position.y * mapSize.y;
-    },
-    init() {
-        this.state.robotPosition.y = 3;
-        // let { robotPosition } = this.state;
-        // robotPosition.y = 3;
-    },
-    onCommandUp() {
-        this.history.push(this.state);
-        this.move({ x: this.state.robotPosition.x, y: this.state.robotPosition.y - 1 });
-    },
-    onCommandBack() {
-        this.move(this.history.pop().robotPosition);
-    },
-    availablePosition(newPosition, mapSize) {
-        if (newPosition.x >= 0 && newPosition.x < mapSize.x
-            && newPosition.y >= 0 && newPosition.y < mapSize.y
+    }
+
+    reverse() {
+        this.position = this.history.pop();
+    }
+}
+
+class Map {
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    availablePosition(newPosition) {
+        if (newPosition.x >= 0 && newPosition.x < this.width
+            && newPosition.y >= 0 && newPosition.y < this.height
         ) {
             return true;
         } else {
             return false;
         }
-    },
-    move(newPosition) {
-        if (this.availablePosition(newPosition, this.state.mapSize)) {
-            this.state.robotPosition = newPosition;
-            // this.state = Object.assign({}, this.state, {robotPosition: newPosition});
-            this.render();
-            return true;
-        } else {
-            return false;
-        }
-    },
+    }
+
+    positionToIndex(position) {
+        return position.x + position.y * this.width;
+    }
+}
+
+class Game {
+    constructor() {
+        this.map = new Map(4, 4);
+        this.robot = new MovingObject(null, "R", this.map);
+    }
+
+    start(startPosition) {
+        this.robot.move(startPosition);
+        this.render();
+    }
+
+    onCommandUp() {
+        // this.history.push(this.state);
+        this.robot.move(this.robot.position.getUpPosition());
+        this.render();
+    }
+
+    onCommandBack() {
+        this.robot.reverse();
+        this.render();
+    }
+    
     render() {
-        var mapCells = document.querySelectorAll('.map-cell');
-        var robotIndex = this.toIndex(this.state.robotPosition, this.state.mapSize);
+        let mapCells = document.querySelectorAll('.map-cell');
+        let robotIndex = this.map.positionToIndex(this.robot.position);
         mapCells.forEach((aCell, i) => {
             if (i === robotIndex) {
-                mapCells[i].innerHTML = 'R';
+                mapCells[i].innerHTML = this.robot.icon;
             } else {
                 mapCells[i].innerHTML = '';
             }
         })
-        // for (var i = 0; i < mapCells.length; i++) {
-        //     if (i === robotIndex) {
-        //         mapCells[i].innerHTML = 'R';
-        //     } else {
-        //         mapCells[i].innerHTML = '';
-        //     }
-        // }
     }
 }
 
 
-game.init();
-game.render();
+
+let game = new Game();
+
+game.start(new Position(0, 3));
